b144fee1c46dc9498628681af235ee019af6f5ee
AleksEvaAleks <166129085+AleksEvaAleks@users.noreply.github.com>
2024-06-18 09:13:34 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php header('Content-Type: text/html; charset=utf-8') ;
+  header('Acess-Control-Allow-Origin: *');
+  header('x-author: buh-tsg');
+?> buh-tsg
---
+++ sample/index.php
@@ -0,0 +1,4 @@
+<?php header('Content-Type: text/html; charset=utf-8') ;
+  header('Acess-Control-Allow-Origin: *');
+  header('x-author: buh-tsg');
+?> function task(x){return x*this*this;}
